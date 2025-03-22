87d8c5d8a0ecdac57d803ff30e6b469cab53f5c1
NtoStudy <2943343011@qq.com>
2025-03-22 15:36:21 +0800

    :recycle: feat: 新增群聊分组功能

--- src/components/friend/controlFunction/ContactList/components/GroupChatList.vue
+++ src/components/friend/controlFunction/ContactList/components/GroupChatList.vue
@@ -1,91 +1,135 @@
 <template>
   <div>
-    <div class="contact-item" v-for="(item, index) in groupItems" :key="index" @click="toggleGroupChat(item)">
-      <div class="item-left">
-        <el-icon class="expand-icon" :class="{ 'is-expanded': expandedGroups.includes(item.name) }">
-          <ArrowRight />
-        </el-icon>
-        <span class="contact-name">{{ item.name }}</span>
+    <!-- 置顶群聊 -->
+    <div class="group-category">
+      <div class="contact-item" @click="toggleGroupChat('置顶群聊')">
+        <div class="item-left">
+          <el-icon class="expand-icon" :class="{ 'is-expanded': expandedGroups.includes('置顶群聊') }">
+            <ArrowRight />
+          </el-icon>
+          <span class="contact-name">置顶群聊</span>
+        </div>
+        <div class="message-count">
+          {{ groupChatMap.pinned.groupCount || getCountByName('置顶群聊') || 0 }}
+        </div>
       </div>
-      <div class="message-count" v-if="item.count">
-        {{ item.count }}
+
+      <!-- 置顶群聊列表 -->
+      <div class="friend-list" v-if="expandedGroups.includes('置顶群聊')">
+        <div
+          v-for="(group, groupIndex) in groupChatMap.pinned.chatRoomsList"
+          :key="groupIndex"
+          class="friend-item"
+          @click="$emit('select-group-chat', group)"
+        >
+          <div class="friend-avatar">
+            <img :src="group.avatar || ''" alt="群头像" />
+          </div>
+          <div class="friend-info">
+            <div class="friend-name">{{ group.roomName }}</div>
+          </div>
+        </div>
+        <div class="empty-tip" v-if="!groupChatMap.pinned || groupChatMap.pinned.length === 0">暂无置顶群聊</div>
       </div>
     </div>
 
-    <!-- 置顶群聊列表 -->
-    <div class="friend-list" v-if="expandedGroups.includes('置顶群聊')">
-      <div
-        v-for="(group, groupIndex) in groupChatMap.pinned"
-        :key="groupIndex"
-        class="friend-item"
-        @click="$emit('select-group-chat', group)"
-      >
-        <div class="friend-avatar">
-          <img :src="group.avatar || ''" alt="群头像" />
+    <!-- 我创建的群聊 -->
+    <div class="group-category">
+      <div class="contact-item" @click="toggleGroupChat('我创建的群聊')">
+        <div class="item-left">
+          <el-icon class="expand-icon" :class="{ 'is-expanded': expandedGroups.includes('我创建的群聊') }">
+            <ArrowRight />
+          </el-icon>
+          <span class="contact-name">我创建的群聊</span>
         </div>
-        <div class="friend-info">
-          <div class="friend-name">{{ group.roomName }}</div>
-          <div class="friend-signature">{{ group.description || '暂无群介绍' }}</div>
+        <div class="message-count">
+          {{ groupChatMap.created.groupCount || getCountByName('我创建的群聊') || 0 }}
         </div>
       </div>
-      <div class="empty-tip" v-if="!groupChatMap.pinned || groupChatMap.pinned.length === 0">暂无置顶群聊</div>
+
+      <!-- 我创建的群聊列表 -->
+      <div class="friend-list" v-if="expandedGroups.includes('我创建的群聊')">
+        <div
+          v-for="(group, groupIndex) in groupChatMap.created.chatRoomsList"
+          :key="groupIndex"
+          class="friend-item"
+          @click="$emit('select-group-chat', group)"
+        >
+          <div class="friend-avatar">
+            <img :src="group.avatar || ''" alt="群头像" />
+          </div>
+          <div class="friend-info">
+            <div class="friend-name">{{ group.roomName }}</div>
+          </div>
+        </div>
+        <div class="empty-tip" v-if="!groupChatMap.created || groupChatMap.created.length === 0">暂无创建的群聊</div>
+      </div>
     </div>
 
-    <!-- 我创建的群聊列表 -->
-    <div class="friend-list" v-if="expandedGroups.includes('我创建的群聊')">
-      <div
-        v-for="(group, groupIndex) in groupChatMap.created"
-        :key="groupIndex"
-        class="friend-item"
-        @click="$emit('select-group-chat', group)"
-      >
-        <div class="friend-avatar">
-          <img :src="group.avatar || ''" alt="群头像" />
+    <!-- 我管理的群聊 -->
+    <div class="group-category">
+      <div class="contact-item" @click="toggleGroupChat('我管理的群聊')">
+        <div class="item-left">
+          <el-icon class="expand-icon" :class="{ 'is-expanded': expandedGroups.includes('我管理的群聊') }">
+            <ArrowRight />
+          </el-icon>
+          <span class="contact-name">我管理的群聊</span>
         </div>
-        <div class="friend-info">
-          <div class="friend-name">{{ group.roomName }}</div>
-          <div class="friend-signature">{{ group.description || '暂无群介绍' }}</div>
+        <div class="message-count">
+          {{ groupChatMap.managed.groupCount || getCountByName('我管理的群聊') || 0 }}
         </div>
       </div>
-      <div class="empty-tip" v-if="!groupChatMap.created || groupChatMap.created.length === 0">暂无创建的群聊</div>
+
+      <!-- 我管理的群聊列表 -->
+      <div class="friend-list" v-if="expandedGroups.includes('我管理的群聊')">
+        <div
+          v-for="(group, groupIndex) in groupChatMap.managed.chatRoomsList"
+          :key="groupIndex"
+          class="friend-item"
+          @click="$emit('select-group-chat', group)"
+        >
+          <div class="friend-avatar">
+            <img :src="group.avatar || ''" alt="群头像" />
+          </div>
+          <div class="friend-info">
+            <div class="friend-name">{{ group.roomName }}</div>
+          </div>
+        </div>
+        <div class="empty-tip" v-if="!groupChatMap.managed || groupChatMap.managed.length === 0">暂无管理的群聊</div>
+      </div>
     </div>
 
-    <!-- 我管理的群聊列表 -->
-    <div class="friend-list" v-if="expandedGroups.includes('我管理的群聊')">
-      <div
-        v-for="(group, groupIndex) in groupChatMap.managed"
-        :key="groupIndex"
-        class="friend-item"
-        @click="$emit('select-group-chat', group)"
-      >
-        <div class="friend-avatar">
-          <img :src="group.avatar || ''" alt="群头像" />
+    <!-- 我加入的群聊 -->
+    <div class="group-category">
+      <div class="contact-item" @click="toggleGroupChat('我加入的群聊')">
+        <div class="item-left">
+          <el-icon class="expand-icon" :class="{ 'is-expanded': expandedGroups.includes('我加入的群聊') }">
+            <ArrowRight />
+          </el-icon>
+          <span class="contact-name">我加入的群聊</span>
         </div>
-        <div class="friend-info">
-          <div class="friend-name">{{ group.roomName }}</div>
-          <div class="friend-signature">{{ group.description || '暂无群介绍' }}</div>
+        <div class="message-count">
+          {{ groupChatMap.joined.groupCount || getCountByName('我加入的群聊') || 0 }}
         </div>
       </div>
-      <div class="empty-tip" v-if="!groupChatMap.managed || groupChatMap.managed.length === 0">暂无管理的群聊</div>
-    </div>
 
-    <!-- 我加入的群聊列表 -->
-    <div class="friend-list" v-if="expandedGroups.includes('我加入的群聊')">
-      <div
-        v-for="(group, groupIndex) in groupChatMap.joined"
-        :key="groupIndex"
-        class="friend-item"
-        @click="$emit('select-group-chat', group)"
-      >
-        <div class="friend-avatar">
-          <img :src="group.avatar || ''" alt="群头像" />
+      <!-- 我加入的群聊列表 -->
+      <div class="friend-list" v-if="expandedGroups.includes('我加入的群聊')">
+        <div
+          v-for="(group, groupIndex) in groupChatMap.joined.chatRoomsList"
+          :key="groupIndex"
+          class="friend-item"
+          @click="$emit('select-group-chat', group)"
+        >
+          <div class="friend-avatar">
+            <img :src="group.avatar || ''" alt="群头像" />
+          </div>
+          <div class="friend-info">
+            <div class="friend-name">{{ group.roomName }}</div>
+          </div>
         </div>
-        <div class="friend-info">
-          <div class="friend-name">{{ group.roomName }}</div>
-          <div class="friend-signature">{{ group.description || '暂无群介绍' }}</div>
-        </div>
+        <div class="empty-tip" v-if="!groupChatMap.joined || groupChatMap.joined.length === 0">暂无加入的群聊</div>
       </div>
-      <div class="empty-tip" v-if="!groupChatMap.joined || groupChatMap.joined.length === 0">暂无加入的群聊</div>
     </div>
   </div>
 </template>
@@ -106,20 +150,33 @@ const emit = defineEmits(['select-group-chat'])
 
 const expandedGroups = ref([])
 const groupChatMap = reactive({
-  pinned: [],
-  created: [],
-  managed: [],
-  joined: [],
+  pinned: {},
+  created: {},
+  managed: {},
+  joined: {},
 })
 
+// 根据分类名称获取对应的消息数量
+const getCountByName = (name) => {
+  const item = props.groupItems.find((item) => item.name === name)
+  return item ? item.count : 0
+}
+
 // 切换群聊分类展开/收起状态
-const toggleGroupChat = (group) => {
-  const groupName = group.name
+const toggleGroupChat = (groupName) => {
   const index = expandedGroups.value.indexOf(groupName)
 
   if (index === -1) {
     expandedGroups.value.push(groupName)
-    loadGroupChatLists()
+    // 如果是第一次展开，加载数据
+    if (
+      (groupName === '置顶群聊' && groupChatMap.pinned.length === 0) ||
+      (groupName === '我创建的群聊' && groupChatMap.created.length === 0) ||
+      (groupName === '我管理的群聊' && groupChatMap.managed.length === 0) ||
+      (groupName === '我加入的群聊' && groupChatMap.joined.length === 0)
+    ) {
+      loadGroupChatLists()
+    }
   } else {
     expandedGroups.value.splice(index, 1)
   }
@@ -130,26 +187,31 @@ const loadGroupChatLists = async () => {
   try {
     // 加载置顶群聊
     const pinnedRes = await chatRoomsPinnedGroup()
-    if (pinnedRes.data && pinnedRes.data.code === 200) {
+    console.log('置顶群聊数据:', pinnedRes.data)
+    if (pinnedRes.data.code === 200) {
       groupChatMap.pinned = pinnedRes.data.data || []
+      console.log('置顶群聊数据:', groupChatMap.pinned)
     }
 
     // 加载我创建的群聊
     const createdRes = await chatRoomsCreateGroup()
-    if (createdRes.data && createdRes.data.code === 200) {
+    if (createdRes.data.code === 200) {
       groupChatMap.created = createdRes.data.data || []
+      console.log('我创建的群聊数据:', groupChatMap.created)
     }
 
     // 加载我管理的群聊
     const managedRes = await chatRoomsManageGroup()
-    if (managedRes.data && managedRes.data.code === 200) {
+    if (managedRes.data.code === 200) {
       groupChatMap.managed = managedRes.data.data || []
+      console.log('我管理的群聊数据:', groupChatMap.managed)
     }
 
     // 加载我加入的群聊
     const joinedRes = await chatRoomsJoinGroup()
-    if (joinedRes.data && joinedRes.data.code === 200) {
+    if (joinedRes.data.code === 200) {
       groupChatMap.joined = joinedRes.data.data || []
+      console.log('我加入的群聊数据:', groupChatMap.joined)
     }
   } catch (error) {
     console.error('加载群聊列表失败:', error)
@@ -157,6 +219,7 @@ const loadGroupChatLists = async () => {
 }
 
 onMounted(() => {
+  // 预加载数据
   if (props.groupItems && props.groupItems.length > 0) {
     loadGroupChatLists()
   }
@@ -164,6 +227,10 @@ onMounted(() => {
 </script>
 
 <style lang="scss" scoped>
+.group-category {
+  margin-bottom: 2px;
+}
+
 .contact-item {
   display: flex;
   justify-content: space-between;
